5c2575e7f5dcf2e490349d4e9ff1c605dfabde0f
CJOYBLUV <dave_hemmer@hotmail.com>
2015-07-22 15:43:16 -0700

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  root 'posts#index'
+
+  get 'signup' => 'users#new', as: :new_user
+  post 'signup' => 'users#create', as: :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
+  # resources :posts, only:[:index,:new,:create]
+  resources :posts do
+    resources :votes, :only =>[:create]
+  end
+  resources :users, :only => [:show] do
+    resources :votes, :only =>[:create]
+  end
+
+
+end
